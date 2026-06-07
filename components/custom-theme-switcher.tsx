a1c27f78f901edd9ae9f566f068f735c29b084e9
Chiranjeev-sahu <chiranjeevsahu1904@gmail.com>
2026-06-07 12:00:00 +0530

    update custom theme switcher

--- components/custom-theme-switcher.tsx
+++ components/custom-theme-switcher.tsx
@@ -13,7 +13,7 @@ export const CustomToggle = () => {
     <button
       type="button"
       onClick={toggleTheme}
-      className="group flex h-8 w-8 cursor-pointer select-none items-center justify-center rounded-full bg-background text-zinc-700 transition-all duration-200 [box-shadow:inset_3px_3px_6px_#d1d1d1,inset_-1px_-1px_2px_#d1d1d1] hover:brightness-[1.02] active:scale-[0.98] border-none dark:text-zinc-100 dark:[box-shadow:inset_2px_2px_6px_#222222,inset_-2px_-2px_4px_#121212]"
+      className="group flex h-8 w-8 cursor-pointer select-none items-center justify-center rounded-full bg-background text-zinc-700 transition-all duration-200 [box-shadow:inset_3px_3px_6px_#d1d1d1,inset_-2px_-2px_3px_#ffffff] shadow hover:brightness-[1.02] active:scale-[0.98] border dark:text-zinc-100 dark:[box-shadow:inset_2px_2px_6px_#222222,inset_-2px_-2px_4px_#121212]"
       aria-label="Toggle theme"
       title="Toggle theme"
     >
