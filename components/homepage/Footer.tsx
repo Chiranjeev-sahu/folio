d53b5ca0cf7594d5dcd13cc250e105b6fa8055cf
Chiranjeev-sahu <chiranjeevsahu1904@gmail.com>
2026-06-19 22:37:19 +0530

    fix(footer):remove breakpoint width

--- components/homepage/Footer.tsx
+++ components/homepage/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <>
       <footer className="flex p-4 w-full lg:w-[46%]">
-        <div className="flex p-4 h-38 md:h-64 w-full lg:w-[46%]">
+        <div className="flex p-4 h-38 md:h-64 w-full">
           <div className="inset-0 h-full w-full bg-background bg-[radial-gradient(var(--grid-color)_2px,transparent_2px)] bg-size-[11.5px_11.5px] " />
         </div>{" "}
       </footer>
